882e88446da077d2b290028ce7b4b6508759320e
akirabenj <akirabenj@hotmail.com>
2021-05-08 13:42:12 +0700

    init

---
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.homework
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonA: Button
+    private lateinit var buttonB: Button
+    private lateinit var buttonC: Button
+    private lateinit var textViewA: TextView
+    private lateinit var textViewB: TextView
+    private lateinit var textViewC: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initializeUIElements()
+    }
+
+    //Функция инициализации элементов activity
+    private fun initializeUIElements() {
+        buttonA = findViewById(R.id.buttonA)
+        buttonB = findViewById(R.id.buttonB)
+        buttonC = findViewById(R.id.buttonC)
+
+        textViewA = findViewById(R.id.textViewA)
+        textViewB = findViewById(R.id.textViewB)
+        textViewC = findViewById(R.id.textViewC)
+    }
+
+    private fun setButtonsListeners() {
+        //Обработчик нажатия на кнопку "Задание 1"
+        buttonA.setOnClickListener {
+            
+        }
+
+        //Обработчик нажатия на кнопку "Задание 2"
+        buttonB.setOnClickListener {
+
+        }
+
+        //Обработчик нажатия на кнопку "Задание 3"
+        buttonC.setOnClickListener {
+
+        }
+    }
+}
