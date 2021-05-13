66a44d84d200c840f85b4206941d2df19c68cac1
ReZaCooL17 <github.com/ReZaCooL17>
2021-05-14 02:36:26 +0700

    домашнее задание

---
+++ app/src/main/java/com/example/homework/Product.kt
@@ -0,0 +1,14 @@
+package com.example.homework
+
+class Product {
+    var name = String
+    var price = Double
+
+
+}
+
+fun moneyA(): Double {
+    val rubs = 1000
+    return (rubs * 75).toDouble()
+}
+
